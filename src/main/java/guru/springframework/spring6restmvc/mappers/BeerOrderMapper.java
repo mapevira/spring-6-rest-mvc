acc3c7e833f1b3620a440a068d0f420eddb3a04c
Raul Perez Vicente <r.perez.vicente@ibermatica.com>
2024-09-08 18:13:06 +0200

    419. Create Mappers

---
+++ src/main/java/guru/springframework/spring6restmvc/mappers/BeerOrderMapper.java
@@ -0,0 +1,31 @@
+package guru.springframework.spring6restmvc.mappers;
+
+import guru.springframework.spring6restmvc.entities.*;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+
+/**
+ * Created by jt, Spring Framework Guru.
+ *
+ * @author architecture - raulp
+ * @version 08/09/2024 - 18:09
+ * @since jdk 1.21
+ */
+@Mapper
+public interface BeerOrderMapper {
+
+    @Mapping(target = "beerOrder", ignore = true)
+    BeerOrderShipment beerOrderShipmentDtoToBeerOrderShipment(BeerOrderShipmentDTO beerOrderShipmentDTO);
+
+    BeerOrderShipmentDTO beerOrderShipmentToBeerOrderShipmentDto(BeerOrderShipment beerOrderShipment);
+
+    @Mapping(target = "beerOrder", ignore = true)
+    BeerOrderLine beerOrderLineDtoToBeerOrderLine(BeerOrderLineDTO beerOrderLineDTO);
+
+    BeerOrderLineDTO beerOrderLineToBeerOrderLineDto(BeerOrderLine beerOrderLine);
+
+    BeerOrder beerOrderDtoToBeerOrder(BeerOrderDTO beerOrder);
+
+    BeerOrderDTO beerOrderToBeerOrderDto(BeerOrder beerOrder);
+
+}
